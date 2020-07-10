6b24e88987b3ed5384702a4ac01686d6b2368233
lmFreire <l.matheus.freire10@gmail.com>
2020-07-10 08:49:43 -0300

    Bando de dados H2 e criacao automatica da base de dados

--- src/main/java/com/lmFreire/cursomc/domain/Categoria.java
+++ src/main/java/com/lmFreire/cursomc/domain/Categoria.java
@@ -4,9 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
-import java.util.Objects;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,6 +18,8 @@ public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
